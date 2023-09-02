17fafd2fd21882894b02d53c9d86140961912aa2
Jung-kwanyong <68227577+rhks7410dyd@users.noreply.github.com>
2023-09-02 15:00:03 +0000

    make snippets in code space(230901)

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define endl '\n'
+#define SIZE 
+
+using namespace std;
+
+int dp[SIZE];
+
+int main(){
+    cin.tie(NULL);	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    //continue code. . .
+
+    return 0;
+}
