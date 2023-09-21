0537f8df1e14905cbcd5bcd77aa11f3ce44ea1ac
Jung-kwanyong <rhks7410dyd@naver.com>
2023-09-21 14:34:01 +0000

    unsovled problem(230921)

--- Floyd_Warshall/boj17182.cpp
+++ Floyd_Warshall/boj17182.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #define endl '\n'
+#define INF 987654321
 
 using namespace std;
 
-int N,K;
+int N,K,ans = INF;
 int dis[10][10];
 int adj[10][10];
+bool visit[10];
 
 void floyd_warshall();
+int huristic(int now);
+int backtracking(int now);
 
 int main(){
     cin.tie(NULL);	cout.tie(NULL);
@@ -24,17 +29,41 @@ int main(){
 
     floyd_warshall();
 
-    for(int i = 0 ; i < N ; i++){
-        for(int j = 0 ; j < N ; j++){
-            cout << dis[i][j] << endl;
-        }
-        cout << endl;
-    }
+    visit[K] = true;
+    ans = huristic(K);
+    visit[K] = false;
+
+    ans = min(ans,backtracking(K));
+
+    cout << ans << endl;
 
     return 0;
 }
 
-void floyed_warshall(){
+int backtracking(int now){
+    -
+}
+
+int huristic(int now){
+    int ret = 0,best_path=INF,next_node;
+
+    for(int i = 0 ; i < N ; i++){
+        if(best_path > dis[now][i] && now != i && !visit[i]){
+            best_path = dis[now][i];
+            next_node = i;
+        }
+    }
+
+    if(best_path == INF)    return 0;
+
+    visit[next_node] = true;
+    ret = dis[now][next_node] + huristic(next_node);
+    visit[next_node] = false;
+
+    return ret;
+}
+
+void floyd_warshall(){
     for(int i = 0 ; i < N ; i++){
         for(int j = 0 ; j < N ; j++){
             dis[i][j] = adj[i][j];
