d20ec94e5a1faf052d032aea45461bb587a3ee64
Jung-kwanyong <rhks7410dyd@naver.com>
2023-10-10 14:10:42 +0000

    unsolved problem(231010)

--- Dynamic_Programming/algospot_WILDCARD.cpp
+++ Dynamic_Programming/algospot_WILDCARD.cpp
@@ -1,6 +1,3 @@
-/*
-아래 주석 처리된 코드는 변형 전 완전 탐색 재귀 함수로 작성한 코드이다.
-*/
 #include <iostream>
 #include <vector>
 #include <string>
@@ -13,13 +10,14 @@ string word;
 vector<string> words;
 vector<string> correct_words;
 
-bool recursive(int pos,int now_command);
+int recursive(int pos,int now_command);
+int dp[101][101];
 
 int main(){
 	cin.tie(NULL);
 	cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
+
 	int C;
 	cin >> C;
 	for(int c = 0 ; c < C ; c++){
@@ -35,6 +33,7 @@ int main(){
 		}
 		
 		for(int i = 0 ; i < word_c ; i++){
+			fill(&dp[0][0],&dp[100][101],-1);
 			word = words[i];
 			if(recursive(0,0)){
 				correct_words.push_back(word);
@@ -46,31 +45,22 @@ int main(){
 		for(int i = 0 ; i < correct_words.size() ; i++){
 			cout << correct_words[i] << '\n';
 		}
-		
+
 		correct_words.clear();
 		words.clear();
 	}
 }
 
-bool recursive(int pos,int now_command){
-	if(pos == word.size()){
-		if(wildcard[now_command] == '*' && now_command < wildcard.size() - 1)	return recursive(pos,now_command + 1);
-		return (now_command == wildcard.size())||(wildcard[now_command] == '*' && now_command == wildcard.size() - 1);
-	}
+int recursive(int pos,int now_command){
+	int& ret = dp[pos][now_command];
 
-	switch(wildcard[now_command]){
-		case '*':
-			return recursive(pos+1,now_command) || recursive(pos+1,now_command+1);
-			break;
-		case '?':
-			return recursive(pos+1,now_command+1);
-			break;
-		default:
-			if(word[pos] == wildcard[now_command])	return recursive(pos+1,now_command+1);
-			break;
-	}
-	
-	return false;
+	if(ret != -1)	return ret;
+
+	if(word[pos] == wildcard[now_command])	return ret = recursive(pos+1,now_command+1);
+
+	//
+
+	return ret;
 }
 
 /*
@@ -79,66 +69,4 @@ a**
 1
 a
 위의 테스트 케이스가 작동을 안함.
-*/
-
-/*
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
-string wildcard;
-vector<string> words;
-
-bool recursive(string word,int pos,int now_command);
-
-int main(){
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios_base::sync_with_stdio(false);
-	
-	int C;
-	cin >> C;
-	for(int c = 0 ; c < C ; c++){
-		int word_c;
-		string temp;
-		
-		cin >> wildcard;
-		cin >> word_c;
-		
-		for(int i = 0 ; i < word_c ; i++){
-			cin >> temp;
-			words.push_back(temp);
-		}
-		
-		for(int i = 0 ; i < word_c ; i++){
-			if(recursive(words[i],0,0)){
-				cout << words[i] << '\n';
-			}
-		}
-		
-		words.clear();
-	}
-}
-
-bool recursive(string word,int pos,int now_command){
-	if(pos >= word.size()){
-		return (now_command == wildcard.size())||(wildcard[now_command]=='*' && now_command == wildcard.size()-1);
-	}
-
-	switch(wildcard[now_command]){
-		case '*':
-			return recursive(word,pos+1,now_command) || recursive(word,pos+1,now_command+1);
-			break;
-		case '?':
-			return recursive(word,pos+1,now_command+1);
-			break;
-		default:
-			if(word[pos] == wildcard[now_command])	return recursive(word,pos+1,now_command+1);
-			break;
-	}
-
-	return false;
-}
 */
--- test.cpp
+++ test.cpp
@@ -4,37 +4,11 @@
 
 using namespace std;
 
-int x,y;
-
-int Rev(int n){
-    if(n == 1000)    return 1;
-    
-    int ret = 0;
-    if(n/100 > 0){
-        ret += n/100;
-        ret += ((n/10)%10)*10;
-        ret += (n%10)*100;
-    }
-    else if(n/10 > 0){
-        ret += n/10;
-        ret += (n%10)*10;
-    }
-    else{
-        ret = n;
-    }
-    
-    return ret;
-}
-
 int main(){
     cin.tie(NULL);	cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     //continue code. . .
-    cin >> x >> y;
-    int ans = Rev(Rev(x)+Rev(y));
-    
-    cout << ans << endl;
 
     return 0;
 }
