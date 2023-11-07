fb54b83e60f313f71b39efb3175468e9864ba30f
Jung-kwanyong <rhks7410dyd@naver.com>
2023-11-07 13:47:40 +0000

    new solved problem(231107)

--- Greedy/boj1052.cpp
+++ Greedy/boj1052.cpp
@@ -1,73 +1,74 @@
-//시간 재다가 막혀서 못품
-//중간에 k-n이 더 좋게 먹히는 경우가 생기는 게 아니라면 애매한듯
-//이거 처리 못하겠으면 비트 연산자로 재구현해보기
-//그냥 2진수니까 물병 13개를 8 + 4 + 1로 보면 3개로 생각 가능
 #include <iostream>
+#define endl '\n'
+#define INF 987654321
 
 using namespace std;
 
-int exponent[25];
-
 void Solve();
-int get_ans(int n,int k);
 
-int main()
-{
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main(){
+	cin.tie(NULL);	cout.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	
+
+	//continue code. . .
+
 	Solve();
-	
+
 	return 0;
 }
 
-void Solve()
-{
+void Solve(){
 	int n,k;
 	cin >> n >> k;
-	
-	int temp = 1;
-	for(int i = 0 ; i < 25 ; i++){
-		exponent[i] = temp;
-		temp *= 2;
-	}
-	int ans = k-n;
-	if(n>k)	ans = get_ans(n,k);
-	
-	cout << ans << '\n';
-}
 
-int get_ans(int n,int k)
-{
-	//cout << "----------------\n";
-	//cout << n << ' ' << k << '\n';
-	if(n==k)	return 0;
-	
-	int merge_val;
+	int t=0;	
+	for(int j = 0 ; j < 25 ; j++){
+		if(n & (1<<j))	t++;
+	}
+	if(t <= k){
+		cout << 0 << endl;
+		return;
+	}
+
+	int ans = 0;	
+	bool can_carry = false;
 	for(int i = 0 ; i < 25 ; i++){
-		if(exponent[i] < n && exponent[i+1] > n)	merge_val = exponent[i];
-	}
-	//cout << merge_val << '\n';
-	if(k == 1){
-		return	merge_val*2 - n;
-	}
-	
-	if(n-merge_val < k-1)
-	{
-		while(n-merge_val < k-1){
-			merge_val /= 2;
+		if(n & (1<<i)){
+			n += 1<<i;
+			t = 0;
+			for(int j = 0 ; j < 25 ; j++){
+				if(n & (1<<j))	t++;
+			}
+			ans += 1<<i;
+
+			if(t <= k){
+				break;
+			}
 		}
-		//cout << "case 1 : changed merge_val is " << merge_val << '\n';
-		n -= merge_val;
-		k--;
-		return get_ans(n,k);
 	}
-	else
-	{
-		n -= merge_val;
-		k--;
 
-		return get_ans(n,k);
-	}
+	cout << ans << endl;
 }
+
+/*
+15 3
+15 = 1111(2)
+1 개 사면
+16리터로 16 = 10000(2)라서 1개의 물통으로 가능
+
+13 2
+13 = 1101(2)
+
+1000000 = 1111 0100 0010 0100 0000(2)
++= 2^6
+1111 0100 0010 1000 0000(2)
++= 2^7
+1111 0100 0011 0000 0000(2)
++= 2^8
+1111 0100 0100 0000 0000(2)
++= 2^10 +2^11 +2^12 +2^13
+->
+2^6 * (1+2+..+2^7) - 2^9
+64*255 - 512
+
+*/
