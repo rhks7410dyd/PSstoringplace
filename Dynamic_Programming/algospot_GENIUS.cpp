3ccf24156f3d99c3a7624f6253a42d23beddbab4
Minpepha <rhks7410dyd@naver.com>
2023-12-30 23:35:12 +0900

    unsolved problem(231230)

--- Dynamic_Programming/algospot_GENIUS.cpp
+++ Dynamic_Programming/algospot_GENIUS.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <algorithm>
 #define endl '\n'
 
 using namespace std;
 
 const int SIZE = 50;
 
-int n,k.m;
-int dp[SIZE];
+int n,k,m;
+int dp[5][SIZE];
 int Like_song[10];
+int song_len[SIZE];
+double table[SIZE][SIZE];
 
 void Solve();
 void Input();
+void print_info();
+void recur(int t);
 
 int main(){
 	cin.tie(NULL);	cout.tie(NULL);
@@ -23,3 +28,50 @@ int main(){
 	}
 	return 0;
 }
+
+void Solve(){
+	Input();
+
+	//print_info();
+
+	//
+}
+
+void Input(){
+	cin >> n >> k >> m;
+	for(int i = 0 ; i < n ; i++){
+		cin >> song_len[i];
+	}
+	
+	for(int i = 0 ; i < n ; i++){
+		for(int j = 0 ; j < n ; j++){
+			cin >> table[i][j];
+		}
+	}
+
+	for(int i = 0 ; i < m ; i++){
+		cin >> Like_song[i];
+	}
+}
+
+void print_info(){
+	cout << "\n\n====================\n\n";
+	cout << n << ' ' << k << ' ' << m << endl;
+	for(int i = 0 ; i < n ; i++){
+		cout << song_len[i] << ' ';
+	}
+	cout << endl;
+
+	for(int i = 0 ; i < n ; i++){
+		for(int j = 0 ; j < n ; j++){
+			cout << table[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	for(int i = 0 ; i < n ; i++){
+		cout << Like_song[i] << ' ';
+	}
+	cout << endl;
+}
+
