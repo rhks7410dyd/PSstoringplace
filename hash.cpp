b61ff2d8e9cd79d6f3b3b74beb06a86874f3ec2f
Jung-kwanyong <68227577+rhks7410dyd@users.noreply.github.com>
2024-01-08 14:05:31 +0900

    test

---
+++ hash.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define endl '\n'
+#define INF 987654321
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n = 99;
+
+    for(int i = 0 ; i < n ; i++){
+        cout << i << ' ';
+    }
+
+    return 0;
+}
