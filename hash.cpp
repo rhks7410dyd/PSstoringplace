6b8a8905fb79c065be7622923195dcb9ea6d12e6
rhks7410dyd <rhks7410dyd@naver.com>
2024-02-11 19:52:18 +0900

    setting_on_new_computer(240211)

--- hash.cpp
+++ hash.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 
 int main(){
-    cin.tie(NULL);	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-
+    
     int n = 99;
 
     for(int i = 0 ; i < n ; i++){
